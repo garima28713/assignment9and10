dd48bcd8e4c60a606fd568d7ef7185f875fc0eab
garima28713 <garima28713@gmail.com>
2023-07-05 12:28:47 +0530

    assignment10 reactive form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.scss']
+  styleUrls: ['./reactive-form.component.scss'],
 })
 export class ReactiveFormComponent {
+  signUpForm = new FormGroup(
+    {
+      fullName: new FormControl<string>(''),
+      email: new FormControl<string>(''),
+      phoneNo: new FormControl<number>(0),
+      address: new FormControl<string>(''),
+      city: new FormControl<string>(''),
+      password: new FormControl<string>(''),
+      confirmPwd: new FormControl<string>(''),
+      agreement: new FormControl<boolean>(false)
+    }
+  );
 
+  signUp(){
+    console.log(this.signUpForm.value)
+  }
 }
